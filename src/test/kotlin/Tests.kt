8238262b058ad8563238739319bb30957aea2a82
Duc Anh Le (FG-232) <Duc-Anh.Le@bmw.de>
2022-11-11 10:21:06 +0100

    Initial setup

---
+++ src/test/kotlin/Tests.kt
@@ -0,0 +1,11 @@
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class Tests {
+
+    @Test
+    fun `First Test`() {
+
+    }
+}
