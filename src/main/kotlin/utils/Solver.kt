e1bf173358b90893f9dbe2cfa713983e9f582d7c
Duci Le <ducanh.le@hotmail.de>
2022-11-25 15:08:53 +0100

    Upgrade solver

--- src/main/kotlin/utils/Solver.kt
+++ src/main/kotlin/utils/Solver.kt
@@ -1,11 +1,33 @@
 package utils
 
 abstract class Solver(val day: Int) {
-    fun solve() {
+    private fun solve() {
         val input = readInputs(day)
 
         println("Part 1: ${solvePart1(input)}")
         println("Part 2: ${solvePart2(input)}")
+    }
+
+    private fun verify(testData: String, expectedOutcome: Int, part: String) {
+        if (part == "#1") {
+            val result = solvePart1(testData)
+
+            if (result.toString() != expectedOutcome.toString()) {
+                error("Part 1 should result in $expectedOutcome but was $result")
+            }
+        } else {
+            val result = solvePart2(testData)
+
+            if (result.toString() != expectedOutcome.toString()) {
+                error("Part 2 should result in $expectedOutcome but was $result")
+            }
+        }
+    }
+
+    fun verifyAndSolve(testData: String, expectedOutcome: Int, part: String) {
+        verify(testData, expectedOutcome, part)
+
+        solve()
     }
 
     abstract fun solvePart1(input: String): Any
