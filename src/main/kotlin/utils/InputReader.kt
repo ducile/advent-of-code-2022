b37accb378263ae2d588eeda4fc47edb0d348a2b
Duci Le <ducanh.le@hotmail.de>
2022-11-30 17:15:42 +0100

    Update input reader

--- src/main/kotlin/utils/InputReader.kt
+++ src/main/kotlin/utils/InputReader.kt
@@ -6,6 +6,6 @@ fun readInputs(day: Int): String {
     return File("data/day${day}_input").readText()
 }
 
-fun readInputLines(day: Int) : List<String> {
-    return readInputs(day).lines()
+fun readInputLines(input: String) : List<String> {
+    return input.lines()
 }
