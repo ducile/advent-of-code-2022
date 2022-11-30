0617f9a415f696e96583e51bd2213199cc8abf6f
Duci Le <ducanh.le@hotmail.de>
2022-11-30 17:13:29 +0100

    Update Solver

--- src/main/kotlin/utils/Solver.kt
+++ src/main/kotlin/utils/Solver.kt
@@ -1,33 +1,39 @@
 package utils
 
-abstract class Solver(val day: Int) {
-    private fun solve() {
+abstract class Solver(private val day: Int) {
+    private fun solve(part: String) {
         val input = readInputs(day)
 
-        println("Part 1: ${solvePart1(input)}")
-        println("Part 2: ${solvePart2(input)}")
+        if (part == "#1")
+            println("Part 1: ${solvePart1(input)}")
+        else
+            println("Part 2: ${solvePart2(input)}")
     }
 
-    private fun verify(testData: String, expectedOutcome: Int, part: String) {
+    fun verify(testData: String, expectedOutcome: Long, part: String) {
         if (part == "#1") {
             val result = solvePart1(testData)
 
             if (result.toString() != expectedOutcome.toString()) {
                 error("Part 1 should result in $expectedOutcome but was $result")
             }
+
+            println("Test result: $result")
         } else {
             val result = solvePart2(testData)
 
             if (result.toString() != expectedOutcome.toString()) {
                 error("Part 2 should result in $expectedOutcome but was $result")
             }
+
+            println("Test result: $result")
         }
     }
 
-    fun verifyAndSolve(testData: String, expectedOutcome: Int, part: String) {
+    fun verifyAndSolve(testData: String, expectedOutcome: Long, part: String) {
         verify(testData, expectedOutcome, part)
 
-        solve()
+        solve(part)
     }
 
     abstract fun solvePart1(input: String): Any
